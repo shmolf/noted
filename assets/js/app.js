4717d808244fafefd2d9e75744f5c0ac532610a7
Nicholas Browning <shmolf@gmail.com>
2021-04-16 23:07:51 +0000

    👾 Transition to Yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,7 @@ import $ from 'jquery';
 import 'CSS/app.scss';
 import M from 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
-// import 'w3-css/w3.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'font-awesome-animation/font-awesome-animation.scss';
 
 import MakeClickable from 'JS/ada/EmulateButton';
 
