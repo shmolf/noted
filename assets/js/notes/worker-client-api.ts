624379511253342f09311df52ad0954a31d0e070
Nicholas Browning <shmolf@gmail.com>
2021-05-30 23:36:19 +0000

    style(whitespace): cleanup worker-client-api

--- assets/js/notes/worker-client-api.ts
+++ assets/js/notes/worker-client-api.ts
@@ -10,77 +10,77 @@ export interface Note extends MapStringTo<any> {
 }
 
 export const clientActions = Object.freeze({
-  GET_LIST: {
-    k: 'getNoteList',
-    f: (): string => packAction(clientActions.GET_LIST.k),
-  },
-  MODIFY: {
-    k: 'modify',
-    f: (note: NotePackage): string => packAction(clientActions.MODIFY.k, note),
-  },
-  RECYCLE: {
-    k: 'recycle',
-    f: (uuid: string): string => packAction(clientActions.RECYCLE.k, uuid),
-  },
-  GET_BY_CLIENTUUID: {
-    k: 'retrieveByUuid',
-    f: (uuid: string):string => packAction(clientActions.GET_BY_CLIENTUUID.k, uuid),
-  },
-  DEL_BY_CLIENTUUID: {
-    k: 'deleteByUuid',
-    f: (uuid: string): string => packAction(clientActions.DEL_BY_CLIENTUUID.k, uuid),
-  },
-  EXPORT_NOTES: {
-    k: 'exportNotes',
-    f: (): string => packAction(clientActions.EXPORT_NOTES.k),
-  }
+    GET_LIST: {
+        k: 'getNoteList',
+        f: (): string => packAction(clientActions.GET_LIST.k),
+    },
+    MODIFY: {
+        k: 'modify',
+        f: (note: NotePackage): string => packAction(clientActions.MODIFY.k, note),
+    },
+    RECYCLE: {
+        k: 'recycle',
+        f: (uuid: string): string => packAction(clientActions.RECYCLE.k, uuid),
+    },
+    GET_BY_CLIENTUUID: {
+        k: 'retrieveByUuid',
+        f: (uuid: string):string => packAction(clientActions.GET_BY_CLIENTUUID.k, uuid),
+    },
+    DEL_BY_CLIENTUUID: {
+        k: 'deleteByUuid',
+        f: (uuid: string): string => packAction(clientActions.DEL_BY_CLIENTUUID.k, uuid),
+    },
+    EXPORT_NOTES: {
+        k: 'exportNotes',
+        f: (): string => packAction(clientActions.EXPORT_NOTES.k),
+    },
 });
 
 export const workerStates = Object.freeze({
-  TEST: {
-    k: 'test',
-    f: (): string => packState(workerStates.TEST.k),
-  },
-  TEST_READY: {
-    k: 'test-ready',
-    f: (): string => packState(workerStates.TEST_READY.k),
-  },
-  READY: {
-    k: 'ready',
-    f: (): string => packState(workerStates.READY.k),
-  },
-  NOTE_LIST: {
-    k: 'note-list',
-    f: (list: any[]): string => packState(workerStates.NOTE_LIST.k, list),
-  },
-  NOTE_DATA: {
-    k: 'note-data',
-    f: (note: NotePackage): string => packState(workerStates.NOTE_DATA.k, note),
-  },
-  UPD8_COMP: {
-    k: 'note-updated',
-    f: (response: any): string => packState(workerStates.UPD8_COMP.k, response),
-  },
-  DEL_COMP: {
-    k: 'note-deleted',
-    f: (uuid: string): string => packState(workerStates.DEL_COMP.k, uuid),
-  },
-  EXPORT_DATA: {
-    k: 'export-data',
-    f: (notes: NotePackage[]): string => packState(workerStates.EXPORT_DATA.k, notes),
-  },
+    TEST: {
+        k: 'test',
+        f: (): string => packState(workerStates.TEST.k),
+    },
+    TEST_READY: {
+        k: 'test-ready',
+        f: (): string => packState(workerStates.TEST_READY.k),
+    },
+    READY: {
+        k: 'ready',
+        f: (): string => packState(workerStates.READY.k),
+    },
+    NOTE_LIST: {
+        k: 'note-list',
+        f: (list: any[]): string => packState(workerStates.NOTE_LIST.k, list),
+    },
+    NOTE_DATA: {
+        k: 'note-data',
+        f: (note: NotePackage): string => packState(workerStates.NOTE_DATA.k, note),
+    },
+    UPD8_COMP: {
+        k: 'note-updated',
+        f: (response: any): string => packState(workerStates.UPD8_COMP.k, response),
+    },
+    DEL_COMP: {
+        k: 'note-deleted',
+        f: (uuid: string): string => packState(workerStates.DEL_COMP.k, uuid),
+    },
+    EXPORT_DATA: {
+        k: 'export-data',
+        f: (notes: NotePackage[]): string => packState(workerStates.EXPORT_DATA.k, notes),
+    },
 });
 
 function packAction(action: string, data?: any): string {
-  return stringIt({ action, data });
+    return stringIt({ action, data });
 }
 
 function packState(state: string, data?: any): string {
-  return stringIt({ state, data });
+    return stringIt({ state, data });
 }
 
 function stringIt(obj: Object): string {
-  return JSON.stringify(obj);
+    return JSON.stringify(obj);
 }
 
 export interface NotePackageExport extends MapStringTo<any> {
