867090f34f7bee8dc26593e9c7e092a2598d841e
Nicholas Browning <shmolf@gmail.com>
2022-07-06 13:07:55 -0400

    feat(images): add support for images

--- webpack.config.js
+++ webpack.config.js
@@ -101,6 +101,15 @@ Encore
 
     .addLoader({ test: /\.md$/, loader: 'raw-loader' })
     .addPlugin(new ESLintPlugin({}))
+    .copyFiles({
+        from: './assets/images',
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
 ;
 
 const config = Encore.getWebpackConfig();
