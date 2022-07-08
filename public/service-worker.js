bb160bbebe3bf6f2aaf37f39aaa80f28ff21fc5c
Nicholas Browning <shmolf@gmail.com>
2022-07-08 17:20:33 -0400

    fix(pwa): enable caching so the app identifies as being offline-able

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,35 @@
-self.addEventListener('install', () => {});
+const PRECACHE = 'precache-v3.20220708';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+    '/public/noted-logo-animated.gif',
+    '/public/noted-logo-static.gif',
+    '/public/noted-logo-static-96.png',
+    '/public/noted-logo-static-144.png',
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+
+  event.waitUntil(
+    caches.keys()
+      .then((names) => names.filter(name => !currentCaches.includes(name)))
+      .then(
+        (cachesToDelete) => Promise.all(
+          cachesToDelete.map((cacheToDelete) => caches.delete(cacheToDelete))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
